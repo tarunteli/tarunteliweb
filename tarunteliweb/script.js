388c4f202ffaaefb906428cad012614e57e638df
tarunteli <tarundeveloper66@gmail.com>
2025-03-13 21:38:45 -0700

    Delete tarunteliweb directory

--- tarunteliweb/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const contactButtons = document.querySelectorAll(".contact-btn");
-
-    contactButtons.forEach(button => {
-        button.addEventListener("click", function () {
-            alert("Redirecting to: " + this.href);
-        });
-    });
-});
